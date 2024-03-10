389b9850817fd8cc53c05bd48e107bda4179683e
seaside1 <joseph@zoidberg.se>
2024-03-10 15:45:40 +0100

    Updated to new way of handling dynamic rules

--- src/main/java/org/openhab/automation/jrule/rules/user/SonosAudioClipRules.java
+++ src/main/java/org/openhab/automation/jrule/rules/user/SonosAudioClipRules.java
@@ -1,29 +1,38 @@
 package org.openhab.automation.jrule.rules.user;
 
+import org.openhab.automation.jrule.internal.engine.JRuleInvocationCallback;
 import org.openhab.automation.jrule.rules.JRule;
+import org.openhab.automation.jrule.rules.event.JRuleEvent;
 import org.openhab.automation.jrule.rules.event.JRuleItemEvent;
 
-public class SonosAudioClipRules extends JRule {
+public class SonosAudioClipRules extends JRule implements JRuleInvocationCallback {
 
   
-    public SonosAudioClipRules() {
-        super(false);
-    }
     
     public void fireAudioClip(JRuleItemEvent event) {
-        final String stateValue = event.getState() == null ? "" : event.getState().toString();
-        logInfo("Executing fireAudioClip event item {} value: {}", event.getItem().getName(), stateValue);
-        final String ip = SonosCoordinator.get().getIpFromUriItem(event.getItem().getName());
+    }
+
+    @Override
+    public void accept(JRuleEvent event) {
+        final String stateValue = ((JRuleItemEvent)event).getState() == null ? "" : ((JRuleItemEvent)event).getState().toString();
+        logInfo("Executing fireAudioClip event item {} value: {}", ((JRuleItemEvent)event).getItem().getName(), stateValue);
+        final String ip = SonosCoordinator.get().getIpFromUriItem(((JRuleItemEvent)event).getItem().getName());
         if (ip == null) {
-            logInfo("Failed to find ip for item, cannot play clip. Item: {}", event.getItem().getName());
+            logInfo("Failed to find ip for item, cannot play clip. Item: {}", ((JRuleItemEvent)event).getItem().getName());
             return;
         }
-        final SonosDeviceInfo deviceInfo = SonosCoordinator.get().getDeviceInfo(event.getItem().getName());
-        String uri = event.getState().stringValue();
+        final SonosDeviceInfo deviceInfo = SonosCoordinator.get().getDeviceInfo(((JRuleItemEvent)event).getItem().getName());
+        String uri = ((JRuleItemEvent)event).getState().stringValue();
         String udn = deviceInfo.getUdn();
         final String volume =  SonosCoordinator.get().getVolume(deviceInfo);
         logInfo("Sending Play Clip ip: {} uri: {} udn: {} volume: {}", ip, uri, udn, volume);
         SonosCoordinator.get().playAudioClip(deviceInfo, uri, volume);
+
+    }
+    
+    @Override
+    public String getRuleLogName() {
+        return "fireAudioClip";
     }
     
 }
